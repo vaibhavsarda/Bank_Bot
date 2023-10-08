e85eadfd2330d6be8111e3fe580a97175a84cbbb
Vaibhav Sarda <vaibhav17320@iiitd.ac.in>
2023-10-09 00:44:48 +0530

    Remove alerts from new.js and photo.js and correct typo in ques.html

--- HSBC_Bot/static/js/new.js
+++ HSBC_Bot/static/js/new.js
@@ -12,12 +12,10 @@ noButton.addEventListener("click", () => {
 
 
 function onYesClick() {
-    alert("you clicked yes");
     window.location.href = '/HSBC_Bot_Server/get_financial_recommendation/';
 }
 
 function onNoClick() {
-    alert("you clicked no");
     window.location.href = "/HSBC_Bot_Server/";
     // Remove the question and everything inside the 'questionDiv'
     // var questionDiv = document.getElementById("questionDiv");
--- HSBC_Bot/static/js/photo.js
+++ HSBC_Bot/static/js/photo.js
@@ -9,11 +9,9 @@ decline.addEventListener("click", () => {
 });
 
 function accept_photo(){
-    alert("you clicked yes");
     window.location.href = "/HSBC_Bot_Server/get_photo/";
 
 }
 function decline_photo(){
-    alert("you clicked no");
     window.location.href ='/HSBC_Bot_Server/yes_no_question/';
 }
